6bc3eb69eff46bb8409f2eed6b4065067a2ffb13
HoussemMhiri <houssemmhiri95@gmail.com>
2023-08-08 15:00:07 +0100

    Weather-App

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const url = "https://wft-geo-db.p.rapidapi.com/v1/geo/cities";
+export const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "19e71425bcmshc379ece11c51288p1450abjsn697e10dce706",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "0db944cc791789df0b423303cae0632e";
